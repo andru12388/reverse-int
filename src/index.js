7bc29d10e29b040d6d576974bbecb90145df17d6
Aliseyeu Andrei <andru12388@tut.by>
2021-08-10 22:49:51 +0300

    fix: file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
-}
+	let str = n.toString();
+	let len = str.length
+	let i = 0;
+	let sum = '';
+	if (n < 0) {
+		while (i <= len - 1) {
+		sum += str[str.length - i];
+		i++;
+		}
+	}
+	else {
+		while (i <= len) {
+		sum += str[str.length - i];
+		i++;
+		}
+	}
+	
+	let substring = sum.slice(9);
+	let reversed = Number(substring);
+	return reversed;
+};
